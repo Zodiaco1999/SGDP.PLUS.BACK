f10af7a84fda9e23f0eb48209e2dd7ee2d79c00d
Brayan Styd Baez Mejia <BSbaez@agoracsc.com>
2023-11-08 10:55:04 -0500

    update mensaje de validacion

--- SEG.MENU/Aplicacion/Funcionalidades/Autenticacion/LogicaNegocio/GestionAutenticacion.cs
+++ SEG.MENU/Aplicacion/Funcionalidades/Autenticacion/LogicaNegocio/GestionAutenticacion.cs
@@ -17,6 +17,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using SGDP.PLUS.Comun.Excepcion;
+using System.Text.RegularExpressions;
 
 namespace SGDP.PLUS.SEG.Aplicacion.Funcionalidades.Autenticacion.LogicaNegocio;
 
@@ -262,7 +263,9 @@ public class GestionAutenticacion : BaseAppService, IGestionAutenticacion
             .FirstOrDefaultAsync() ?? throw new NotFoundException(nameof(Usuario), "El usuario no existe");
 
         if (!HashCustom.CheckHash(registroDto.PasswordActual, user.Contrasena, user.Salt))
-            throw new Exception("Contraseña actual invalida");
+            throw new ValidationException("Contraseña actual invalida");
+        else if (!new Regex(_parametrosSEG.PatronContrasena).IsMatch(registroDto.PasswordNueva))
+            throw new ValidationException(_parametrosSEG.MensajeValidacionContrasena);
         else
         {
             var hash = HashCustom.Hash(registroDto.PasswordNueva);
@@ -300,7 +303,7 @@ public class GestionAutenticacion : BaseAppService, IGestionAutenticacion
         }
         catch (Exception ex)
         {
-            throw new BadRequestCustomException($"No se logro enviar el correo.", ex);
+            throw new BadRequestCustomException($"No se logro enviar el correo. Si el error persiste contacte a soporte", ex);
         }
 
         user.Token = token;
@@ -322,6 +325,9 @@ public class GestionAutenticacion : BaseAppService, IGestionAutenticacion
 
         if (!registroDto.PasswordNueva.Equals(registroDto.PasswordConfirmacion))
             throw new ValidationException("Las contraseñas no coinciden");
+
+        else if (!new Regex(_parametrosSEG.PatronContrasena).IsMatch(registroDto.PasswordNueva))
+            throw new ValidationException(_parametrosSEG.MensajeValidacionContrasena);
 
         var hash = HashCustom.Hash(registroDto.PasswordNueva);
         user.Contrasena = hash.Password;
